af0538d4c1f4727457ae2c119711219323f418da
Discord9 <discord9@163.com>
2022-06-18 21:06:29 +0800

    add SOI/EOI(find bug)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+use pest::Parser;
+mod test;
+#[derive(Parser)]
+#[grammar = "../pest/rinja.pest"]
+pub struct RinjaParser;
 fn main() {
+    let res = RinjaParser::parse(Rule::tmpl_unit, "123");
+    println!("{:?}",res);
     println!("Hello, world!");
 }
---
+++ src/test.rs
@@ -0,0 +1,32 @@
+#![cfg(test)]
+extern crate pest;
+use crate::pest::{Parser};
+#[derive(Parser)]
+#[grammar = "../pest/rinja.pest"]
+pub struct RinjaParser;
+
+#[test]
+fn manual_test(){
+    // simple string
+    let res = RinjaParser::parse(Rule::tmpl_unit, "123");
+    println!("{:?}", res);
+    assert!(res.is_ok());
+
+    // simple expr
+    let res = RinjaParser::parse(Rule::tmpl_unit, "123{{a}}bcd");
+    assert!(res.is_ok());
+    if res.is_err(){
+        println!("{:?}", res.unwrap().as_str());
+    }
+    
+
+    // simple expr that fail
+    let res = RinjaParser::parse(Rule::tmpl_unit, "123{{1a}}bcd");
+    assert!(res.is_err());
+    if res.is_ok(){
+        println!("{:?}", res.expect_err("Should fail as illegal ident"));
+    }
+
+    let res = RinjaParser::parse(Rule::tmpl_unit, "{% for a in b %}{% endfor %}");
+    println!("{:?}", res);
+}
