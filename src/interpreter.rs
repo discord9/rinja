ba924b1843074bb317cf79fcfaa5111998e6c901
Discord9 <discord9@163.com>
2022-06-19 19:10:03 +0800

    render var into tmpl

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -44,12 +44,14 @@ lazy_static! {
 pub trait Visitor<T, E> {
     //fn visit_block(&mut self, s:&Pairs<Rule>)->T;
     fn visit_expr(&mut self, s: Pairs<Rule>) -> Result<T, E>;
+    fn visit_tmpl_unit(&mut self, s: Pairs<Rule>);
 }
 
 type BuiltIn = Box<dyn Fn(Vec<value::Value>) -> value::Value>;
 struct Interpreter {
     env: value::Value, // not ref because it may be modify by {% set lvalue = expr %}
     built_in_fn: HashMap<String, BuiltIn>,
+    render_result: String,
 }
 
 impl Interpreter {
@@ -59,7 +61,11 @@ impl Interpreter {
             "existsIn".to_string(),
             Box::new(|args: Vec<Value>| -> Value { args.get(0).unwrap().to_owned() }) as BuiltIn,
         )]);
-        Self { env, built_in_fn }
+        Self {
+            env,
+            built_in_fn,
+            render_result: String::new(),
+        }
     }
 }
 impl Visitor<value::Value, RendererError<'static>> for Interpreter {
@@ -72,10 +78,14 @@ impl Visitor<value::Value, RendererError<'static>> for Interpreter {
                 Rule::ident => match self.env.get(pair.as_str()) {
                     Some(v) => v.to_owned(),
                     None => {
-                        let (l,c) = pair.as_span().start_pos().line_col();
+                        let (l, c) = pair.as_span().start_pos().line_col();
                         // Should panic because its interpreting so recover from error is not a good idea?
-                        panic!("Variable {} not found!(At {}:{})", pair.as_str()
-                        .to_string(), l, c);
+                        panic!(
+                            "Variable {} not found!(At {}:{})",
+                            pair.as_str().to_string(),
+                            l,
+                            c
+                        );
                     }
                 },
                 Rule::expr => self.visit_expr(pair.into_inner()).unwrap(),
@@ -114,13 +124,50 @@ impl Visitor<value::Value, RendererError<'static>> for Interpreter {
         };
         Ok(PREC_CLIMBER.climb(expr, primary, infix))
     }
+    fn visit_tmpl_unit(&mut self, unit: Pairs<Rule>) {
+        //println!("Unit:{:?}", unit);
+        let unit = unit.to_owned().next().unwrap();
+        println!("Unit:{:?}", unit);
+        self.render_result = String::with_capacity(unit.as_str().len() * 2);
+        for tmpl_section in unit.into_inner() {
+            match tmpl_section.as_rule() {
+                Rule::tmpl_literal => self.render_result.push_str(tmpl_section.as_str()),
+                Rule::expr => {
+                    let eval_res = self.visit_expr(tmpl_section.into_inner()).unwrap();
+                    if eval_res.is_string() {
+                        self.render_result.push_str(eval_res.as_str().unwrap());
+                    } else if eval_res.is_number() {
+                        let res = if eval_res.is_i64() {
+                            eval_res.as_i64().unwrap().to_string()
+                        } else if eval_res.is_f64() {
+                            eval_res.as_f64().unwrap().to_string()
+                        }else{unimplemented!()};
+                        self.render_result.push_str(format!("{}", res).as_str())
+                    } else {
+                        unimplemented!()
+                    }
+                }
+                Rule::EOI => continue,
+                _ => {
+                    println!("{:?} Not yet support!", tmpl_section.as_rule());
+                    unimplemented!()
+                },
+            }
+        }
+    }
 }
 
 #[test]
 fn test_num_expr() {
     use crate::{Parser, RinjaParser};
     let res = RinjaParser::parse(Rule::expr, "1+a*3^2");
-    println!("{:?}", res);
+    //println!("{:?}", res);
     let mut interp = Interpreter::new(serde_json::from_str(r#"{"a":42}"#).unwrap());
     println!("{:?}", interp.visit_expr(res.unwrap()));
+    
+    let res = RinjaParser::parse(Rule::tmpl_unit, "aa{{ a }}bb");
+    println!("{:?}", res.to_owned().unwrap());
+    let mut interp = Interpreter::new(serde_json::from_str(r#"{"a":42}"#).unwrap());
+    interp.visit_tmpl_unit(res.unwrap());
+    println!("Renderer Result: {}", interp.render_result);
 }
